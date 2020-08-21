9a32c666d821a929cd420a67d5236d8da9cb9e70
Jonathan Leitschuh <Jonathan.Leitschuh@gmail.com>
2020-08-21 13:45:26 -0400

    Create basic project structure

---
+++ src/test/java/org/jlleitschuh/sandbox/TempDirectoryPermissionsCheck.java
@@ -0,0 +1,50 @@
+package org.jlleitschuh.sandbox;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TempDirectoryPermissionsCheck {
+    @Test
+    public void checkTempDefaultPermissions() throws IOException {
+        File temp = File.createTempFile("random", "file");
+        runLS(temp.getParentFile());
+    }
+
+    private static void runLS(File file) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        processBuilder.command("ls", "-l", file.getAbsolutePath());
+
+        try {
+            Process process = processBuilder.start();
+
+            StringBuilder output = new StringBuilder();
+
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+                System.exit(0);
+            } else {
+                //abnormal...
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
